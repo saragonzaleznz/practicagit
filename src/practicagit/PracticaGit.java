c89ca2f29c59ebe82c6b1f2c99dfe1fe9356bbdd
saragonzaleznz <45658080+saragonzaleznz@users.noreply.github.com>
2022-03-06 22:25:03 +0100

    Segunda vez

--- src/practicagit/PracticaGit.java
+++ src/practicagit/PracticaGit.java
@@ -14,6 +14,7 @@ public class PracticaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+     String p = "hola";
         // TODO code application logic here
     }
     
